08c13d1876aecd5778bbebad8e51e0567a33c650
Michael Ray-Von <mikey@interglobal.vision>
2016-06-08 16:48:42 -0500

    add logo and nav menu

--- header.php
+++ header.php
@@ -57,5 +57,51 @@
 
   <!-- start content -->
   <header id="header">
+  <?php
+    $logo = IGV_get_option('_igv_metadata_logo');
+
+    if ($logo) {
+  ?>
+    <a href="<?php echo home_url(); ?>"><img src="<?php echo $logo; ?>" class="logo"></a>
+  <?php
+    } else {
+  ?>
     <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+  <?php } 
+
+    $args = array(
+      'posts_per_page'   => 2,
+      'post_type'        => 'editorial',
+    );
+    $editorial_posts = get_posts( $args );
+  ?>
+
+  <nav>
+    <ul class="menu">
+    <?php if ( class_exists( 'WooCommerce' ) ) { ?>
+      <li class="menu-item">
+        <a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>">Shop</a>
+      </li>
+    <?php } 
+
+      if ($editorial_posts) {
+    ?>
+      <li class="menu-item">
+        <a href="<?php echo get_permalink($editorial_posts[0]->ID); ?>">Editorial</a>
+      </li>
+    <?php
+        if (count($editorial_posts) > 1) {
+    ?>
+      <li class="menu-item">
+        <a href="<?php echo get_post_type_archive_link( 'editorial' ); ?>">Archive</a>
+      </li>
+    <?php 
+        } 
+      }
+    ?>
+      <li class="menu-item">
+        <a href="<?php echo get_permalink( get_option('page_for_posts' ) ); ?>">Journal</a>
+      </li>
+    </ul>
+  </nav>
   </header>
