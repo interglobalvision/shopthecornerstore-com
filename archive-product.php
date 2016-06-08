b4deae2b2b628737014c2f0f1bf0a9b4ac2103c4
Michael Ray-Von <mikey@interglobal.vision>
2016-06-08 17:53:09 -0500

    remove woo templates. replace archive-product template with our own homegrown

--- archive-product.php
+++ archive-product.php
@@ -1,107 +1,51 @@
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
- * will need to copy the new files to your theme to maintain compatibility. We try to do this.
- * as little as possible, but it does happen. When this occurs the version of the template file will.
- * be bumped and the readme will list any important changes.
- *
- * @see 	    http://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.0
- */
+get_header();
+?>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+<!-- main content -->
 
-get_header( 'shop' ); ?>
+<main id="main-content">
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+  <!-- main posts loop -->
+  <section id="posts" class="row">
 
-		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
 
-			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+    $stock = get_post_meta($post->ID, '_stock_status', true);
+?>
 
-		<?php endif; ?>
+    <article <?php post_class('col col-3'); ?> id="post-<?php the_ID(); ?>">
 
-		<?php
-			/**
-			 * woocommerce_archive_description hook.
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
-			do_action( 'woocommerce_archive_description' );
-		?>
+      <?php if ($stock === 'outofstock') { ?>
+      <span class="sold">Sold</span>
+      <?php } ?>
 
-		<?php if ( have_posts() ) : ?>
+      <?php the_post_thumbnail(); ?>
 
-			<?php
-				/**
-				 * woocommerce_before_shop_loop hook.
-				 *
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-			?>
+      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 
-			<?php woocommerce_product_loop_start(); ?>
+    </article>
 
-				<?php woocommerce_product_subcategories(); ?>
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+  <!-- end posts -->
+  </section>
 
-					<?php wc_get_template_part( 'content', 'product' ); ?>
+  <?php get_template_part('partials/pagination'); ?>
 
-				<?php endwhile; // end of the loop. ?>
+<!-- end main-content -->
 
-			<?php woocommerce_product_loop_end(); ?>
+</main>
 
-			<?php
-				/**
-				 * woocommerce_after_shop_loop hook.
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop' );
-			?>
-
-		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-		<?php endif; ?>
-
-	<?php
-		/**
-		 * woocommerce_after_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-	<?php
-		/**
-		 * woocommerce_sidebar hook.
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-	?>
-
-<?php get_footer( 'shop' ); ?>
+<?php
+get_footer();
+?>
