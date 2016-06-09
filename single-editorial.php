c4e7bc9c851dc23676c9d79631d9cf2ae759c362
Michael Ray-Von <mikey@interglobal.vision>
2016-06-09 15:14:55 -0500

    add single editorial template with basic swiper markup

---
+++ single-editorial.php
@@ -0,0 +1,54 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+  <!-- main posts loop -->
+  <section id="posts">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+      <!-- Slider main container -->
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide">Slide 1</div>
+          <div class="swiper-slide">Slide 2</div>
+          <div class="swiper-slide">Slide 3</div>
+        </div>
+
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+
+  <!-- end posts -->
+  </section>
+
+  <?php get_template_part('partials/pagination'); ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
