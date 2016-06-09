3794c0052f73f6c13d31a484b891603af049eb7c
Michael Ray-Von <mikey@interglobal.vision>
2016-06-09 14:32:10 -0500

    add filter to change woocommerce out of stock text

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -1,3 +1,13 @@
 <?php
 
 // Custom filters (like pre_get_posts etc)
+
+add_filter( 'woocommerce_get_availability', 'igv_outofstock_text', 1, 2);
+function igv_outofstock_text( $availability, $_product ) {
+   
+  // Change Out of Stock Text
+  if ( ! $_product->is_in_stock() ) {
+    $availability['availability'] = __('Sold', 'woocommerce');
+  }
+  return $availability;
+}
