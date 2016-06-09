b06148d5478c95d1821b354c3ffd699731fc6bc4
Michael Ray-Von <mikey@interglobal.vision>
2016-06-09 14:57:47 -0500

    add titles and featured images to editorial archive template

--- archive-editorial.php
+++ archive-editorial.php
@@ -7,29 +7,49 @@ get_header();
 <main id="main-content">
 
   <!-- main posts loop -->
-  <section id="posts">
+  <section id="posts" class="row">
 
 <?php
 if( have_posts() ) {
+?>
+    <div class="col col6">
+      <ul>
+<?php
   while( have_posts() ) {
     the_post();
 ?>
 
-    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-      <?php the_content(); ?>
-
-    </article>
+        <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
 
 <?php
   }
+?>
+
+      </ul>
+    </div>
+
+    <div class="col col6">
+
+<?php
+  while( have_posts() ) {
+    the_post();
+?>
+
+    <?php the_post_thumbnail(); ?>
+
+<?php
+  }
+?>
+
+    </div>
+<?php
 } else {
 ?>
     <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
+    
+
 
   <!-- end posts -->
   </section>
