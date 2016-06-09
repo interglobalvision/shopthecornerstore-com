0119c1585a71c1933fc2094729bace73c191bde9
Michael Ray-Von <mikey@interglobal.vision>
2016-06-09 18:01:05 -0500

    add swiper init and product details functions

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ Site = {
       _this.onResize();
     });
 
+    if ($('.swiper-container').length) {
+      _this.initSwiper();
+    }
+
   }, 
 
   onResize: function() {
@@ -24,6 +28,48 @@ Site = {
       string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
       $(this).html(string);
     });
+  },
+
+  initSwiper: function() {
+    var _this = this;
+
+    var swiper = new Swiper('.swiper-container', {
+      preloadImages: false,
+      lazyLoading: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      onInit: function(swiper) {
+        _this.updateProductDetails();
+      },
+      onSlideChangeStart: function(swiper) {
+        $('.product-details').hide();
+      },
+      onSlideChangeEnd: function(swiper) {
+        _this.updateProductDetails();
+      }
+    });
+
+  },
+
+  updateProductDetails: function() {
+    if ($('.swiper-slide-active').attr('data-product')) {
+      var productData = JSON.parse($('.swiper-slide-active').attr('data-product'));
+
+      $('.js-product-title').html(productData['title']).attr('href',productData['url']);
+      $('.js-product-content').html(productData['content']);
+      $('.js-product-price').html(productData['price']);
+
+      if (productData['stock']) {
+        $('.js-product-button').html(productData['button_text']).removeClass('u-hidden');
+        $('.js-product-id').attr('value',productData['id']);
+        $('.js-product-sold').addClass('u-hidden');
+      } else {
+        $('.js-product-sold').removeClass('u-hidden').html(productData['availability']['availability']);
+        $('.js-product-button').addClass('u-hidden');
+      }
+
+      $('.product-details').show();
+    } 
   },
 };
 
