4393ed890f288c63f0b92ade3a70a369a05f3e8b
Michael Ray-Von <mikey@interglobal.vision>
2016-06-09 16:11:15 -0500

    add product info to slide data attributes

--- single-editorial.php
+++ single-editorial.php
@@ -1,5 +1,12 @@
 <?php
 get_header();
+
+$args = array(
+  'post_type' => 'editorial',
+  'post_per_page' => 1,
+);
+$recent_editorial = get_posts($args);
+$recent_id = $recent_editorial[0]->ID;
 ?>
 
 <!-- main content -->
@@ -13,22 +20,56 @@ get_header();
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
+
+    $slides = get_post_meta($post->ID, '_igv_slides');
+    $is_recent = ($recent_id === $post->ID ? true : false);
 ?>
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+<?php 
+    if ($slides) {
+?>
 
       <!-- Slider main container -->
       <div class="swiper-container">
         <div class="swiper-wrapper">
           <!-- Slides -->
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
+<?php 
+      foreach($slides[0] as $slide) {
+        $has_product = ($is_recent && $slide['product'] ? true : false);
+
+        if ($has_product) {
+          $product = new WC_Product($slide['product']);
+          $title = $product->get_title();
+          $price = $product->get_price_html();
+          $in_stock = $product->check_stock_status();
+          $availability = $product->get_availability();
+          $cart_url = $product->add_to_cart_url();
+          $cart_text = $product->single_add_to_cart_text();
+        }
+?>
+          <div class="swiper-slide" <?php if ($has_product) {
+            echo 'data-product="true"';
+            echo 'data-title="' . $title . '"';
+            echo 'data-price="' . htmlspecialchars($price) . '"';
+            echo 'data-stock="' . $in_stock . '"';
+            echo 'data-availability="' . $availability['availability'] . '"';
+            echo 'data-cart-url="' . $cart_url . '"';
+            echo 'data-cart_text="' . $cart_text . '"';
+          } else {
+            echo 'data-product="false"';
+          } ?> >
+            <?php echo $slide['image']?>
+          </div>
+<?php } ?>
         </div>
 
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
       </div>
+
+<?php } ?>
 
     </article>
 
