81a667669bc270608fc699a7e5f1f94f2f70f033
Michael Ray-Von <mikey@interglobal.vision>
2016-06-21 18:15:19 -0500

    add pre_get_posts filter for editorial archive latest post

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -11,3 +11,21 @@ function igv_outofstock_text( $availability, $_product ) {
   }
   return $availability;
 }
+
+// exclude most recent editorial fron archive
+function exclude_recent_editorial( $query ) {
+  if ( is_admin() || ! $query->is_main_query() )
+    return;
+
+  if ( $query->is_post_type_archive('editorial') ) {
+    $args = array(
+      'post_type' => 'editorial',
+      'post_per_page' => 1,
+    );
+    $recent_editorial = get_posts($args);
+    $recent_id = $recent_editorial[0]->ID;
+    
+    $query->set( 'post__not_in', array( $recent_id ) );
+  }
+}
+add_action( 'pre_get_posts', 'exclude_recent_editorial', 1 );
