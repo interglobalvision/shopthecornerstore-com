10fce467df7a2dbff74e62ff227821fdd7278a9d
Michael Ray-Von <mikey@interglobal.vision>
2016-06-27 19:26:44 -0500

    move nav into partial. add splash partial on front page

--- header.php
+++ header.php
@@ -55,7 +55,11 @@
 
   <section id="main-container">
 
-  <?php if (is_front_page()) { get_template_part('partials/splash'); } ?>
+<?php 
+  if (is_front_page()) { 
+    get_template_part('partials/splash'); 
+  } else { 
+?>
 
   <!-- start content -->
   <header id="header" class="container">
@@ -75,41 +79,9 @@
   <?php }
     ?>
       </div>
-  <?php
-    $args = array(
-      'posts_per_page'   => 2,
-      'post_type'        => 'editorial',
-    );
-    $editorial_posts = get_posts( $args );
-  ?>
-      <nav class="col col6">
-        <ul id="menu" class="u-inline-list">
-        <?php if ( class_exists( 'WooCommerce' ) ) { ?>
-          <li class="menu-item">
-            <a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>">Shop</a>
-          </li>
-        <?php }
 
-          if ($editorial_posts) {
-        ?>
-          <li class="menu-item">
-            <a href="<?php echo get_permalink($editorial_posts[0]->ID); ?>">Editorial</a>
-          </li>
-        <?php
-            if (count($editorial_posts) > 1) {
-        ?>
-          <li class="menu-item">
-            <a href="<?php echo get_post_type_archive_link( 'editorial' ); ?>">Archive</a>
-          </li>
-        <?php
-            }
-          }
-        ?>
-          <li class="menu-item">
-            <a href="<?php echo get_permalink( get_option('page_for_posts' ) ); ?>">Journal</a>
-          </li>
-        </ul>
-      </nav>
+    <?php get_template_part('partials/nav'); ?>
+
   <?php
     if (is_singular('editorial')) {
   ?>
@@ -121,3 +93,5 @@
   ?>
     </div>
   </header>
+
+<?php } ?>
