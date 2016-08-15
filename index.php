059eadff038d6b1747445d600110d93d1cd37ace
mikey ray-von <mikey@interglobal.vision>
2016-08-15 14:00:00 -0700

    add journal-post item selector class to masonry items

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if( have_posts() ) {
     the_post();
 ?>
 
-      <article <?php post_class('col col-s-12 col-m-6'); ?> id="post-<?php the_ID(); ?>">
+      <article <?php post_class('journal-post col col-s-12 col-m-6'); ?> id="post-<?php the_ID(); ?>">
 
         <?php the_content(); ?>
 
@@ -33,7 +33,7 @@ if( have_posts() ) {
 <?php 
 } else {
 ?>
-    <article class="u-alert col col-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+    <article class="u-alert journal-post col col-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
 
