8b899fe521b47dbe06deef18c7bab3a38b3d6f53
mikey ray-von <mikey@interglobal.vision>
2016-08-15 13:18:22 -0700

    update elem classes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if( have_posts() ) {
     the_post();
 ?>
 
-      <article <?php post_class('journal-post'); ?> id="post-<?php the_ID(); ?>">
+      <article <?php post_class('col col-s-12 col-m-6'); ?> id="post-<?php the_ID(); ?>">
 
         <?php the_content(); ?>
 
@@ -30,11 +30,10 @@ if( have_posts() ) {
   }
 ?>
     </div>
-    <div class="text-align-center"><a class="button js-load-more">Load More</a></div>
 <?php 
 } else {
 ?>
-    <article class="u-alert masonry-item"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+    <article class="u-alert col col-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
 
