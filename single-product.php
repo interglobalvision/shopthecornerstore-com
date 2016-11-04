645b0ee687ac713e4b38195dc51d27f8e359eda9
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-11-04 16:46:37 -0600

    move slider pagination into single templates. fix mobile slider dom order

--- single-product.php
+++ single-product.php
@@ -20,9 +20,9 @@ if( have_posts() ) {
 
 ?>
 
-    <article <?php post_class('row'); ?> id="post-<?php the_ID(); ?>">
+    <article <?php post_class('row slider-row'); ?> id="post-<?php the_ID(); ?>">
 
-      <div class="col col-l-3 col-no-margin-bottom column justify-between">
+      <div class="col col-s-12 col-l-3 col-no-margin-bottom column justify-between">
 
         <div class="slider-product-details margin-top-small">
 
@@ -33,9 +33,18 @@ if( have_posts() ) {
     if (!empty($slides)) {
       if (count($slides[0] > 1)) {
 ?>
-        <div class="slider-pagination-holder margin-top-small margin-bottom-basic row align-end">
+        <div class="slider-pagination-holder margin-top-small margin-bottom-basic row align-end only-desktop">
 
-          <?php get_template_part( 'partials/slider-pagination' ); ?>
+          <div class="col col-s-4 col-no-margin-bottom col-no-gutter">
+            <button class="slider-button slider-prev">
+              <?php echo file_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/arrow_left.svg'); ?>
+            </button>
+          </div>
+          <div class="col col-s-4 col-no-margin-bottom col-no-gutter">
+            <button class="slider-button slider-next">
+              <?php echo file_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/arrow_right.svg'); ?>
+            </button>
+          </div>
 
         </div>
 <?php 
@@ -45,9 +54,17 @@ if( have_posts() ) {
       </div>
 <?php
   if (!empty($slides)) {
-    //pr($slides); die;
+    if (count($slides[0] > 1)) {
 ?>
-      <div class="col col-l-9 col-no-margin-bottom">
+          <div class="col col-s-1 col-no-gutter mobile-only row align-center justify-center">
+            <button class="slider-button slider-prev">
+              <?php echo file_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/arrow_left.svg'); ?>
+            </button>
+          </div>
+<?php 
+      }
+?>
+      <div class="col col-s-10 col-l-9 col-no-margin-bottom">
         <!-- Slider main container -->
         <div class="swiper-container set-swiper-height">
           <div class="swiper-wrapper align-center">
@@ -82,6 +99,15 @@ if( have_posts() ) {
         <!-- End Slider main container -->
       </div>
 <?php
+    if (count($slides[0] > 1)) {
+?>
+          <div class="col col-s-1 col-no-gutter mobile-only row align-center justify-center">
+            <button class="slider-button slider-next">
+              <?php echo file_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/arrow_right.svg'); ?>
+            </button>
+          </div>
+<?php 
+    }
   }
 ?>
 
