89d8c662d9668b4a628f5fdacf916c9cb1fd41a5
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-11-14 16:39:58 -0600

    unenqueue woocommerce select2

--- functions.php
+++ functions.php
@@ -70,6 +70,18 @@ function cmb_initialize_cmb_meta_boxes() {
 
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
+add_action( 'wp_enqueue_scripts', 'mgt_dequeue_stylesandscripts', 100 );
+function mgt_dequeue_stylesandscripts() {
+    if ( class_exists( 'woocommerce' ) ) {
+        wp_dequeue_style( 'select2' );
+        wp_deregister_style( 'select2' );
+
+        wp_dequeue_script( 'select2');
+        wp_deregister_script('select2');
+
+    } 
+} 
+
 // Add custom functions
 
 get_template_part( 'lib/functions-misc' );
