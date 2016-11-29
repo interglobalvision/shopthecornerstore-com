94ed71d8a86c03c90b5d346051db66e7a1c89a4b
Patrick Best <patrickbest@patrickbest.com>
2016-11-29 12:53:01 -0600

    update metabox instructions

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -83,7 +83,7 @@ function igv_cmb_metaboxes() {
 
   $editorial_group->add_field( array(
     'name'       => __( 'Credits', 'cmb2' ),
-    'desc'       => __( '', 'cmb2' ),
+    'desc'       => __( 'These show as the caption to the last slide. Make sure the last slide does not have any products attached otherwise the credits will not show', 'cmb2' ),
     'id'         => $prefix . 'credits_text',
     'type'       => 'wysiwyg',
   ) );
@@ -92,7 +92,7 @@ function igv_cmb_metaboxes() {
   $editorial_group_field_id = $editorial_group->add_field( array(
     'id'          => $prefix . 'slides',
     'type'        => 'group',
-    'description' => __( '', 'cmb2' ),
+    'description' => __( 'Make sure the last slide does not have any products attached otherwise the credits will not show', 'cmb2' ),
     'options'     => array(
       'group_title'   => __( 'Slide {#}', 'cmb2' ), // {#} gets replaced by row number
       'add_button'    => __( 'Add Another Slide', 'cmb2' ),
