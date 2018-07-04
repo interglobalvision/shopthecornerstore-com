567ab60e0d866377bdb02b210e5e432a86d896d6
Cas <cas@interglobal.vision>
2018-07-04 15:10:20 -0500

    add lazysizes class to images

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -9,7 +9,7 @@ function igv_stock_text( $availability, $_product ) {
   if ( $_product->is_in_stock() ) {
     $availability['availability'] = __('In stock', 'woocommerce');
   }
-   
+
   // Change Out of Stock Text
   if ( ! $_product->is_in_stock() ) {
     $availability['availability'] = __('Sold', 'woocommerce');
@@ -29,7 +29,7 @@ function exclude_recent_editorial( $query ) {
     );
     $recent_editorial = get_posts($args);
     $recent_id = $recent_editorial[0]->ID;
-    
+
     $query->set( 'post__not_in', array( $recent_id ) );
   }
 }
@@ -45,3 +45,29 @@ function change_cart_text( $translated_text, $text, $domain ){
   return $translated_text;
 }
 add_filter( 'gettext', 'change_cart_text', 10, 3 );
+
+// Custom img attributes to be compatible with lazysize
+function add_lazysize_on_srcset($attr) {
+	if (!is_admin()) {
+		// if image has data-no-lazysizes attribute dont add lazysizes classes
+		if (isset($attr['data-no-lazysizes'])) {
+			unset($attr['data-no-lazysizes']);
+			return $attr;
+		}
+		// Add lazysize class
+		$attr['class'] .= ' lazyload';
+		if (isset($attr['srcset'])) {
+			// Add lazysize data-srcset
+			$attr['data-srcset'] = $attr['srcset'];
+			// Remove default srcset
+			unset($attr['srcset']);
+		} else {
+			// Add lazysize data-src
+			$attr['data-src'] = $attr['src'];
+		}
+		// Set default to white blank
+		$attr['src'] = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=';
+	}
+	return $attr;
+}
+add_filter('wp_get_attachment_image_attributes', 'add_lazysize_on_srcset');
