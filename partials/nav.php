2281ec65da81b2e165ff31b2b018a1f60f15b363
mikeyrayvon <mikey@interglobal.vision>
2018-07-05 16:17:28 -0500

    remove archive section & rename pages

--- partials/nav.php
+++ partials/nav.php
@@ -12,23 +12,17 @@
 <ul id="menu" class="u-inline-list <?php echo is_front_page() ? 'font-nav-splash' : 'font-nav'; ?>">
 <?php if ( class_exists( 'WooCommerce' ) ) { ?>
   <li class="menu-item <?php echo is_post_type_archive('product') || is_singular('product') ? 'active' : ''; ?>">
-    <a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>">Shop</a>
+    <a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>">Vintage</a>
   </li>
-<?php }
+<?php
+  }
 
   if ($editorial_posts) {
 ?>
   <li class="menu-item <?php echo is_singular('editorial') && $post->ID === $recent_id ? 'active' : ''; ?>">
-    <a href="<?php echo get_permalink($editorial_posts[0]->ID); ?>">Editorial</a>
+    <a href="<?php echo get_permalink($editorial_posts[0]->ID); ?>">Collection</a>
   </li>
 <?php
-    if (count($editorial_posts) > 1) {
-?>
-  <li class="menu-item <?php echo is_post_type_archive('editorial') || is_singular('editorial') && $post->ID !== $recent_id ? 'active' : ''; ?>">
-    <a href="<?php echo get_post_type_archive_link( 'editorial' ); ?>">Archive</a>
-  </li>
-<?php
-    }
   }
 ?>
   <li class="menu-item <?php echo is_home() ? 'active' : ''; ?>">
