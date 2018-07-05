06a7a14af12e3623e4fe768ed5651afb63ec70c5
mikeyrayvon <mikey@interglobal.vision>
2018-07-05 17:54:02 -0500

    use splash video

--- partials/splash.php
+++ partials/splash.php
@@ -1,9 +1,19 @@
 <div class="splash-container">
 <?php
+$splash = IGV_get_option('_igv_splash_option');
+$mp4 = IGV_get_option('_igv_splash_video_mp4');
+$webm = IGV_get_option('_igv_splash_video_webm');
 $images = IGV_get_option('_igv_splash_images');
 $color = IGV_get_option('_igv_splash_color');
 
-if (!empty($images)) {
+if ($splash === 'video' && !empty($mp4) && !empty($webm)) {
+?>
+  <video muted autoplay loop id="splash-video">
+    <source src="<?php echo $mp4 ?>" type="video/mp4">
+    <source src="<?php echo $webm ?>" type="video/webm">
+  </video>
+<?php
+} else if ($splash === 'images' && !empty($images)) {
 ?>
   <div class="splash-swiper swiper-container">
     <div class="swiper-wrapper">
@@ -24,7 +34,7 @@ if (!empty($images)) {
     <div class="container">
       <div class="row">
         <a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' ) ); ?>" class="col col-s-12 col-m-8 offset-m-2 splash-logo" <?php echo !empty($color) ? 'style="fill:' . $color . ';"' : ''; ?>>
-          <?php echo '<img src="' . get_bloginfo('stylesheet_directory') . '/img/dist/logo_white.svg">'; ?>
+          <?php get_template_part('partials/logo'); ?>
         </a>
 
         <nav class="col col-s-12 col-m-8 offset-m-2 splash-nav">
