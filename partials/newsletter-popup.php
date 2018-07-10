3683847a5b0a52a7942e169682924cddaf34ef3b
Cas <cas@interglobal.vision>
2018-07-09 20:37:08 -0500

    scaffold newsletter popup and functionality

---
+++ partials/newsletter-popup.php
@@ -0,0 +1,11 @@
+<div class="newsletter-popup row justify-center align-center">
+  <div class="col col-s-10 col-m-4 col-l-3 text-align-center">
+    <span class="font-uppercase">Sign up to our mailing list to be the first to receive weekly updates on new items</span>
+    <form class="newsletter-form">
+      <input class="newsletter-email" type="email" />
+      <div><button type="submit">Subscribe</button></div>
+      <span class="newsletter-reply"></span>
+    </form>
+    <button id="close-popup">close</button>
+  </div>
+</div>
