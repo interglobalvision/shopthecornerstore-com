df3e1157a3f1400365a7ddeb527222798c0191e9
mikeyrayvon <mikey@interglobal.vision>
2018-07-13 15:29:13 -0500

    fix footer nav marginand container"

--- footer.php
+++ footer.php
@@ -8,43 +8,45 @@
     $contact_id = get_id_by_slug('contact');
     $shipping_id = get_id_by_slug('shipping');
   ?>
-    <footer id="footer" class="container padding-top-tiny padding-bottom-tiny only-desktop">
-      <div class="row align-center">
-        <div class="col col-no-margin-bottom col-s-6">
-          <?php if (!empty($facebook) || !empty($twitter) || !empty($instagram)) { ?>
-          <ul class="u-inline-list">
-            <?php if (!empty($facebook)) { ?>
-            <li>
-              <a href="<?php echo $facebook; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/facebook_icon.svg"></a>
-            </li>
-            <?php } if (!empty($instagram)) { ?>
-            <li>
-              <a href="https://instagram.com/<?php echo $instagram; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/instagram_icon.svg"></a>
-            </li>
-            <?php } if (!empty($twitter)) { ?>
-            <li>
-              <a href="https://twitter.com/<?php echo $twitter; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/twitter_icon.svg"></a>
-            </li>
-            <?php } ?>
-          </ul>
-          <?php } ?>
-        </div>
-        <div class="col col-no-margin-bottom col-s-6 text-align-right">
-          <?php if (!empty($contact_id) || !empty($shipping_id)) { ?>
-          <nav>
-            <ul class="u-inline-list font-nav">
-              <?php if (!empty($contact_id)) { ?>
+    <footer id="footer" class="only-desktop">
+      <div class="container">
+        <div class="row align-center padding-top-tiny padding-bottom-tiny">
+          <div class="col col-no-margin-bottom col-s-6">
+            <?php if (!empty($facebook) || !empty($twitter) || !empty($instagram)) { ?>
+            <ul class="u-inline-list">
+              <?php if (!empty($facebook)) { ?>
               <li>
-                <a href="<?php echo get_the_permalink($contact_id); ?>">Contact</a>
+                <a href="<?php echo $facebook; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/facebook_icon.svg"></a>
               </li>
-              <?php } if (!empty($shipping_id)) { ?>
+              <?php } if (!empty($instagram)) { ?>
               <li>
-                <a href="<?php echo get_the_permalink($shipping_id); ?>">Shipping</a>
+                <a href="https://instagram.com/<?php echo $instagram; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/instagram_icon.svg"></a>
+              </li>
+              <?php } if (!empty($twitter)) { ?>
+              <li>
+                <a href="https://twitter.com/<?php echo $twitter; ?>"><img class="social-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/dist/twitter_icon.svg"></a>
               </li>
               <?php } ?>
             </ul>
-          </nav>
-          <?php } ?>
+            <?php } ?>
+          </div>
+          <div class="col col-no-margin-bottom col-s-6 text-align-right">
+            <?php if (!empty($contact_id) || !empty($shipping_id)) { ?>
+            <nav>
+              <ul class="u-inline-list font-nav">
+                <?php if (!empty($contact_id)) { ?>
+                <li class="menu-item">
+                  <a href="<?php echo get_the_permalink($contact_id); ?>">Contact</a>
+                </li>
+                <?php } if (!empty($shipping_id)) { ?>
+                <li class="menu-item">
+                  <a href="<?php echo get_the_permalink($shipping_id); ?>">Shipping</a>
+                </li>
+                <?php } ?>
+              </ul>
+            </nav>
+            <?php } ?>
+          </div>
         </div>
       </div>
     </footer>
