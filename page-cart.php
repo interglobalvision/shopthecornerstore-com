acf2c36319e8658101745a0a25279fc7474b8cc4
mikeyrayvon <mikey@interglobal.vision>
2018-09-25 18:02:49 -0500

    update cart markup

--- page-cart.php
+++ page-cart.php
@@ -12,19 +12,15 @@ if( have_posts() ) {
     the_post();
 ?>
   <div class="gws-cart">
-    <div id="cart" class="row">
-    	<div class="col col-s-1 col-m-1 product-remove">&nbsp;</div>
-    	<div class="col col-s-2 col-m-2 product-thumbnail">&nbsp;</div>
-    	<div class="col col-s-4 col-m-6 product-name"><?php _e( 'Product', 'woocommerce' ); ?></div>
-    	<div class="col col-s-2 col-m-1 product-price"><?php _e( 'Price', 'woocommerce' ); ?></div>
-    	<div class="col col-s-2 col-m-1 product-subtotal"><?php _e( 'Total', 'woocommerce' ); ?></div>
+    <div id="cart-header" class="row font-nav margin-bottom-tiny">
+    	<div class="col col-s-12 text-align-center padding-bottom-small">BASKET</div>
     </div>
 
     <div id="cart-items" class="gws-cart-items">
-      <div class="row gws-cart-product">
+      <div class="row gws-cart-item padding-bottom-tiny margin-bottom-tiny">
         <div class="col col-s-1 col-m-1 row justify-center align-center">
           <div class="col col-no-gutter col-no-margin-bottom">
-            <a class="gws-cart-remove">&times;</a>
+            <a class="gws-cart-remove u-pointer">&times;</a>
           </div>
         </div>
 
@@ -32,9 +28,14 @@ if( have_posts() ) {
 
         <div class="col col-s-4 col-m-6 gws-cart-title"></div>
 
-        <div class="col col-s-2 col-m-1 gws-cart-price"></div>
-
         <div class="col col-s-2 col-m-1 gws-cart-subtotal"></div>
+      </div>
+    </div>
+
+    <div id="cart-empty" class="row">
+      <div class="col">
+        <div class="margin-bottom-small"><span>Your basket is currently empty</span><div>
+        <a class="button" href="<?php echo home_url('shop'); ?>">Return To Shop </a>
       </div>
     </div>
   </div>
