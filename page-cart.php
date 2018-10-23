faa34d75923a194fd03b237136772f7a70e23069
mikeyrayvon <mikey@interglobal.vision>
2018-10-23 16:25:24 -0500

    wrap cart items and footer in cart-body

--- page-cart.php
+++ page-cart.php
@@ -16,32 +16,36 @@ if( have_posts() ) {
     	<div class="col col-s-12 text-align-center padding-bottom-small">BASKET</div>
     </div>
 
-    <div id="cart-items" class="gws-cart-items">
-      <div class="row gws-cart-item padding-bottom-tiny margin-bottom-tiny align-center">
-        <div class="col text-align-center">
-          <a class="gws-cart-remove u-pointer">&times;</a>
+    <div id="cart-body">
+      <div id="cart-items" class="gws-cart-items">
+        <div class="row gws-cart-item padding-bottom-tiny margin-bottom-tiny align-center">
+          <div class="col text-align-center">
+            <a class="gws-cart-remove u-pointer">&times;</a>
+          </div>
+
+          <div class="col"><div class="gws-cart-thumb"></div></div>
+
+          <div class="col flex-grow gws-cart-title"></div>
+
+          <div class="col text-align-right">$ <span class="gws-cart-item-subtotal"></span></div>
         </div>
+      </div>
 
-        <div class="col"><div class="gws-cart-thumb"></div></div>
-
-        <div class="col flex-grow gws-cart-title"></div>
-
-        <div class="col text-align-right">$ <span class="gws-cart-item-subtotal"></span></div>
+      <div id="cart-footer" class="row padding-top-tiny text-align-right">
+        <div class="col col-s-12 padding-bottom-small">
+          SUBTOTAL: $ <span id="gws-cart-subtotal"></span>
+        </div>
+        <div class="col col-s-12 padding-bottom-small">
+          <a href="" class="button gws-checkout-link">Proceed to Checkout</a>
+        </div>
       </div>
     </div>
 
-    <div id="cart-footer" class="row padding-top-tiny text-align-right">
-      <div class="col col-s-12 padding-bottom-small">
-        SUBTOTAL: $ <span id="gws-cart-subtotal"></span>
-      </div>
-      <div class="col col-s-12 padding-bottom-small">
-        <a href="" class="button gws-checkout-link">Proceed to Checkout</a>
-      </div>
-    </div>
-
-    <div id="cart-empty" class="row">
-      <div class="col">
-        <div class="margin-bottom-small"><span>Your basket is currently empty</span><div>
+    <div id="cart-empty" class="row padding-top-tiny justify-center">
+      <div class="col text-align-center">
+        <div class="margin-bottom-small">
+          <span>Your basket is currently empty</span>
+        </div>
         <a class="button" href="<?php echo home_url('shop'); ?>">Return To Shop </a>
       </div>
     </div>
